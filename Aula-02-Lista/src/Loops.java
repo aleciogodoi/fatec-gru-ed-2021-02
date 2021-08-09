134981b86f4db776148bd3d5441e4ae15db62f1b
Alécio <alecio.godoi@ymail.com>
2021-08-09 15:21:50 -0300

    Aula 02

--- Aula-02-Lista/src/Loops.java
+++ Aula-02-Lista/src/Loops.java
@@ -2,14 +2,38 @@ public class Loops {
 	
 	public static void main(String[] args) {
 		imprime();
+		imprime2();
+		imprime3();
+	}
+	
+	public static void imprime3() {
+		int i = 22;
+		do {
+			if(par(i))
+				System.out.println(i+" é par (do...while)");
+			else
+				System.out.println(i+" é ímpar (do...while)");
+			i++;
+		} while (i < 21);
+	}
+	
+	public static void imprime2() {
+		int i = 22;
+		while (i < 21) {
+			if(par(i))
+				System.out.println(i+" é par (while)");
+			else
+				System.out.println(i+" é ímpar (while)");
+			i++;
+		}
 	}
 	
 	public static void imprime() {
 		for (int i=0; i < 21; i++) {
 			if(par(i))
-				System.out.println(i+" é par");
+				System.out.println(i+" é par (for)");
 			else
-				System.out.println(i+" é ímpar");
+				System.out.println(i+" é ímpar (for)");
 		}	
 	}
 	
