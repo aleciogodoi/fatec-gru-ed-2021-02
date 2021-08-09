59ce26d3adf23fc480de05747a5ec75a539a9d74
Alécio <alecio.godoi@ymail.com>
2021-08-09 18:07:33 -0300

    Aula 02

--- Aula-02-Lista/src/Arrays.java
+++ Aula-02-Lista/src/Arrays.java
@@ -15,17 +15,56 @@ public class Arrays {
 		}
 		
 		String[][] jv =  {
-				{".", ".", "."},
-				{".", "X", "."},
-				{".", ".", "."},	
+				{"0", "0", "X"},
+				{"X", "0", "X"},
+				{"0", "X", "X"},	
 		};
-		
+		if (verificaGanhador(jv))  
+			System.out.println("Temos um ganhador");
+		/*
 		for (int linha=0; linha < 3; linha++) {
 			for (int coluna=0; coluna < 3; coluna++) {
 				System.out.print(jv[linha] [coluna]+" ");
 			}
 			System.out.println();
+		}*/
+	}
+	
+	public static boolean verificaGanhador(String[][] j) {
+		boolean ganhou=false;
+		String aux;
+		// Ganhador diagonal
+		aux = j[0][0]+j[1][1]+j[2][2];
+		// Ganhador por linhas
+		if ( aux.equals("XXX") || aux.equals("000") ) {
+			ganhou = true;
+		} else  {
+			for(int lin=0; lin < 3; lin++) {
+				aux="";
+				for(int col=0; col < 3; col++) {
+					aux= aux + j[lin][col];
+				}
+				System.out.println(aux);
+				if (aux.equals("000") || aux.equals("XXX")) {
+					ganhou = true;
+					break;
+				}
+			}
+	
+			// Ganhador por colunas
+			for(int col=0; col < 3; col++) {
+				aux="";
+				for(int lin=0; lin < 3; lin++) {
+					aux= aux + j[lin][col];
+				}
+				System.out.println(aux);
+				if (aux.equals("000") || aux.equals("XXX")) {
+					ganhou = true;
+					break;
+				}
+			}
 		}
+		return ganhou;
 	}
 
 }
